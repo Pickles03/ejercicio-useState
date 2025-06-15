d1c9cdef3bd496d7504914ef2040088f738997f6
Pickles03 <elizaveta.kris@gmail.com>
2025-06-15 20:04:10 +0200

    functionalities done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import './App.css'
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
 
 function App() {
- //Aquí tu código
+    const [name, setName] = useState('Sofia');
+    const [newName, setNewName] = useState('');
+
+    return (
+        <div>
+            <h2>Teacher name: {name}</h2>
+            <ul>
+                <li onClick = {() => setName('Data')}>Data</li>
+                <li onClick = {() => setName('Reyes')}>Reyes</li>
+                <li onClick = {() => setName('Yolanda')}>Yolanda</li>
+            </ul>
+        </div>
+    )
 }
 
-export default App
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById('root')).render(<App />);
